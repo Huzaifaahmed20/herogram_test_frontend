2b5d97aeda33728f79effce9d22ed4e2c3043c18
Huzaifa Ahmed <h.ahmed@easygenerator.com>
2024-12-16 16:02:57 +0400

    update base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "http://159.223.19.203:8080";
 
 export const apiInstance = axios.create({
   baseURL: BASE_URL,
